50885f0cfa9c79d02e24b06f712884a3c25c42df
ueki5 <ueki005@gmail.com>
2022-01-18 00:41:31 +0900

    ステレオ対応

--- code2/code2-5.py
+++ code2/code2-5.py
@@ -4,21 +4,29 @@ import numpy as np
 
 # サンプルwavファイルを開く
 #sample_wave_file="./CMU_ARCTIC/cmu_us_aew_arctic/wav/arctic_a0001.wav"
-#sample_wave_file="./wgn_wave.wav"
-sample_wave_file="./recording.wav"
+sample_wave_file="./wgn_wave.wav"
+#sample_wave_file="./recording.wav"
 wav=wave.open(sample_wave_file)
 
 # 属性を表示
-print("サンプリング周波数[Hz]: ", wav.getframerate())
-print("サンプルサイズ[Bytes]: ", wav.getsampwidth())
-print("サンプル数: ", wav.getnframes())
-print("チャネル数: ", wav.getnchannels())
+frame_rate=wav.getframerate()
+sample_size=wav.getsampwidth()
+frame_count=wav.getnframes()
+channels=wav.getnchannels()
+print("サンプリング周波数[Hz]: ", frame_rate)
+print("サンプルサイズ[Bytes]: ", sample_size)
+print("サンプル数: ", frame_count)
+print("チャネル数: ", channels)
 
 # 音声データを展開
 data=wav.readframes(wav.getnframes())
 data=np.frombuffer(data, dtype=np.int16)
+
+# ステレオの場合、２次元配列に変換
+if channels==2:
+    data=data.reshape(frame_count, channels)
 # 音声データを再生
-sd.play(data, wav.getframerate())
+sd.play(data, frame_rate)
 
 print("再生開始")
 
--- code2/code2-6.py
+++ code2/code2-6.py
@@ -2,12 +2,14 @@ import wave as wave
 import sounddevice as sd
 import numpy as np
 
-wave_length=5
+wave_length=5 # 単位（秒）
 sample_rate=44100
+sample_size=2
+channels=2
 
 print("録音開始")
 
-data=sd.rec(int(wave_length*sample_rate), sample_rate, channels=2)
+data=sd.rec(int(wave_length*sample_rate), samplerate=sample_rate, channels=channels, dtype=np.int16)
 sd.wait()
 
 print("録音終了")
@@ -16,13 +18,16 @@ print("録音終了")
 wave_out=wave.open("./wgn_wave.wav", 'w')
 
 # モノラル:1、ステレオ:2
-wave_out.setnchannels(2)
+wave_out.setnchannels(channels)
 
-# サンプルサイズ設定（2バイト）
-wave_out.setsampwidth(2)
+# サンプルサイズ設定
+wave_out.setsampwidth(sample_size)
 
 # サンプリング周波数
 wave_out.setframerate(sample_rate)
+
+# フレーム数を設定
+wave_out.setnframes(wave_length*sample_rate)
 
 # データを書き込み
 wave_out.writeframes(data)
